681be8edbe8ceb8fcc9543a99d10b34aa6cc6820
Md Nouman <noumantannu06@gmail.com>
2026-04-07 21:55:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function getFormvalue() {
     //Write your code here
+	event.preventDefault();
 
-}
+      const form = document.getElementById("loginForm");
+
+      const firstName = form.fname.value.trim();
+      const lastName = form.lname.value.trim();
+
+      const fullName = [firstName, lastName].filter(Boolean).join(" ");
+
+      alert(fullName);
+    }
+
+
+
